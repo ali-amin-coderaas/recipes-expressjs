1658284a4f038131601f435fd67140b5345e88e2
Ali Amin <prof.aliamin@gmail.com>
2024-08-05 14:38:23 +0300

    Fix database connection issue

--- database.js
+++ database.js
@@ -9,6 +9,9 @@ const pool = mysql
 		user: process.env.MYSQL_USER,
 		password: process.env.MYSQL_PASSWORD,
 		database: process.env.MYSQL_DATABASE,
+		waitForConnections: true,
+		connectionLimit: 10,
+		queueLimit: 0,
 	})
 	.promise();
 
